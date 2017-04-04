341a416fcb7d6276d6c2fcf1aa1fa2cab386ad93
Jonathan Troy <jonrtroy@gmail.com>
2017-04-04 18:00:29 -0400

    delete cors

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const app = express();
 
-const cors = require('cors');
-
-app.use(cors());
-
 app.use(express.static(__dirname + "/dist"));
 
 app.get("/", (req, res) => {
